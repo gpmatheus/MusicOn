eba31318be0ddd54f8aaa1d9f29237c4eb363e62
gpmatheus <gp.matheusluis@gmail.com>
2022-06-26 15:16:27 -0300

    added player bar

--- front-music-on/src/app/app.module.ts
+++ front-music-on/src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, DomSanitizer } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './main-screen/main-screen.component';
+import { PlayerBarComponent } from './main-screen/player-bar/player-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainScreenComponent,
+    PlayerBarComponent
   ],
   imports: [
     BrowserModule,
@@ -18,9 +26,18 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressBarModule,
+    MatSliderModule,
+    HttpClientModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor (private registry: MatIconRegistry, private sanitizer: DomSanitizer) {
+    this.registry.addSvgIcon('pause-icon', this.sanitizer.bypassSecurityTrustResourceUrl('assets/pause.svg'))
+  }
+}
---
+++ front-music-on/src/app/main-screen/player-bar/player-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-player-bar',
+  templateUrl: './player-bar.component.html',
+  styleUrls: ['./player-bar.component.css']
+})
+export class PlayerBarComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
