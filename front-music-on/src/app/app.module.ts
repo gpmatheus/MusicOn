bc5fd530ad5caff9fef1d4eac63db54c38578524
gpmatheus <gp.matheusluis@gmail.com>
2022-06-26 10:17:10 -0300

    refactored

--- front-music-on/src/app/app.module.ts
+++ front-music-on/src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
